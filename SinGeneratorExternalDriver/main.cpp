f62f2dcc9bfdbd72483648688541c6401c797a8e
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2017-08-10 15:43:38 +0200

    used new event handler

--- SinGeneratorExternalDriver/main.cpp
+++ SinGeneratorExternalDriver/main.cpp
@@ -13,142 +13,213 @@ using namespace chaos::micro_unit_toolkit;
 using namespace chaos::micro_unit_toolkit::data;
 using namespace chaos::micro_unit_toolkit::connection;
 using namespace chaos::micro_unit_toolkit::connection::unit_proxy;
+using namespace chaos::micro_unit_toolkit::connection::unit_proxy::raw_driver;
 using namespace chaos::micro_unit_toolkit::connection::protocol_adapter;
 
-int executeWork(RawDriverUnitProxy& proxy);
-int manageRemoteMessage(RawDriverUnitProxy& proxy);
-int performIdleAction(RawDriverUnitProxy& proxy);
-DataPackUniquePtr consumeMessage(DataPackSharedPtr& message);
-bool authorized = false;
+int event_handler(void *user_data, unsigned int event, void *event_data);
 
 int main(int argc, const char * argv[]) {
     int err = 0;
-    bool run = true;
-    bool connecting = false;
     ChaosMicroUnitToolkit mut;
     const char *option="Content-Type: application/bson-json\r\n";
-    ChaosUniquePtr< UnitConnection<RawDriverUnitProxy> > proxy = mut.createNewRawDriverUnit(ProtocolTypeHTTP,
-                                                                                            "ws://localhost:8080/io_driver",
-                                                                                            option);
-    if(proxy.get() == NULL) {std::cerr << "No unit proxy allcoated";return EXIT_FAILURE;}
-    while (run) {
-        //schedule work
-        switch (proxy->protocol_adapter->getConnectionState()) {
-            case ConnectionStateConnecting: {
-                std::cout << "." << std::flush;
-                break;
-            }
-                
-            case ConnectionStateConnected:{
-                std::cout << "Connected" << std::endl;
-                break;
-            }
-                
-            case ConnectionStateDisconnected:
-            case ConnectionStateConnectionError:
-            case ConnectionStateNotAccepted:
-                while (proxy->unit_proxy->hasMoreMessage()) {
-                    std::cout << proxy->unit_proxy->getNextMessage()->message->toString();
-                }
-                if((ConnectionStateDisconnected == proxy->protocol_adapter->getConnectionState()) &&
-                   connecting == false) {
-                    std::cout << "Connecting" << std::flush;
-                    proxy->protocol_adapter->connect();
-                    connecting = true;
-                } else {
-                    connecting = false;
-                    std::cout << "Retry in 5 seconds"<<std::endl;
-                    sleep(5);
-                }
-                break;
-                
-            case ConnectionStateAccepted:{
-                if(connecting) {
-                    std::cout << "Accepted" << std::endl;
-                    connecting = false;
-                }
-                //manage the work on driver
-                if((err = executeWork(*proxy->unit_proxy))) {
-                    std::cerr << "Error " << err << " during work";
-                    run = false;
-                }
-                break;
-            }
-        }
-        
-        //execute pool on connection
-        proxy->protocol_adapter->poll(100);
-    }
+    ChaosUniquePtr<RawDriverHandlerWrapper> hw = mut.createNewRawDriverHandlerWrapper(ProtocolTypeHTTP,
+                                                                                      "ws://localhost:8080/io_driver",
+                                                                                      option,
+                                                                                      event_handler,
+                                                                                      NULL,
+                                                                                      "work");
     
-    return 0;
+    while (err == 0) {
+        err = hw->poll(100);
+    }
+    std::cout << "Exti with code" << err;
+    return err?EXIT_FAILURE:EXIT_SUCCESS;
 }
 
-int executeWork(RawDriverUnitProxy& proxy) {
+int event_handler(void *user_data, unsigned int event, void *event_data) {
     int err = 0;
-    switch (proxy.getAuthorizationState()) {
-        case AuthorizationStateUnknown:
-            authorized = false;
-            std::cout << "Autorizing" << std::flush;
-            proxy.authorization("work");
+    switch(event){
+        case UP_EV_USR_ACTION:{
+            std::cout << "UP_EV_USR_ACTION" << std::endl;
+            sleep(1);
             break;
-        case AuthorizationStateRequested:
-            std::cout << "." << std::flush;
-            proxy.manageAutorizationPhase();
+        }
+        case UP_EV_CONN_CONNECTED:{
+            std::cout << "UP_EV_CONN_CONNECTED" << std::endl;
             break;
-        case AuthorizationStateDenied:
-            std::cout << " authorization failed" << std::endl;
-            return -1;
-        case AuthorizationStateOk:
-            if(authorized == false){
-                std::cout << "Authorized" << std::endl;
-                authorized = true;
+        }
+        case UP_EV_CONN_ERROR:{
+            std::cout << "UP_EV_CONN_ERROR" << std::endl;
+            break;
+        }
+        case UP_EV_CONN_RETRY:{
+            std::cout << "UP_EV_CONN_RETRY" << std::endl;
+            break;
+        }
+        case UP_EV_CONN_DISCONNECTED:{
+            std::cout << "UP_EV_CONN_DISCONNECTED" << std::endl;
+            break;
+        }
+        case UP_EV_CONN_ACCEPTED:{
+            std::cout << "UP_EV_CONN_ACCEPT" << std::endl;
+            break;
+        }
+        case UP_EV_CONN_REJECTED:{
+            std::cout << "UP_EV_CONN_REJECTED" << std::endl;
+            break;
+        }
+        case UP_EV_AUTH_ACCEPTED:{
+            std::cout << "UP_EV_AUTH_ACCEPTED" << std::endl;
+            break;
+        }
+        case UP_EV_AUTH_REJECTED:{
+            std::cout << "UP_EV_AUTH_REJECTED" << std::endl;
+            break;
+        }
+        case UP_EV_MSG_RECEIVED:{
+            std::cout << "UP_EV_MSG_RECEIVED" << std::endl;
+            break;
+        }
+        case UP_EV_REQ_RECEIVED:{
+            std::cout << "UP_EV_REQ_RECEIVED" << std::endl;
+            UPRequest *req = static_cast<UPRequest*>(event_data);
+            switch (req->message->getInt32("opcode")) {
+                case 0:
+                    std::cout << "OPCODE 0 - Create new generation id" << std::endl;
+                    req->response->addInt32("gen_id", 0);
+                    break;
+                    
+                default:
+                    break;
             }
-            //manage request by remote user
-            err = manageRemoteMessage(proxy);
-            //perform idle operation
-            if(!err){
-                err = performIdleAction(proxy);
-            }
             break;
+        }
+        case UP_EV_ERR_RECEIVED:{
+            std::cout << "UP_EV_ERR_RECEIVED" << std::endl;
+            UPError *req = static_cast<UPError*>(event_data);
+            std::cerr << "[Remote Error]\nCode:"<<req->error<<
+            "\nError Message:"<<req->message<<
+            "\nError Domain:"<<req->domain;
+            break;
+        }
     }
     return err;
 }
 
-int manageRemoteMessage(RawDriverUnitProxy& proxy) {
-    RemoteMessageUniquePtr remote_message;
-    DataPackUniquePtr remote_req_resp;
-    while(proxy.hasMoreMessage()) {
-        remote_message = proxy.getNextMessage();
-        if(remote_message->is_request &&
-           remote_message->request_message.get()){
-            remote_req_resp = consumeMessage(remote_message->request_message);
-            proxy.sendAnswer(remote_message, remote_req_resp);
-        } else if(remote_message->isError() == false){
-            remote_req_resp = consumeMessage(remote_message->message);
-        } else {
-            std::cerr << "[Remote Error]\nCode:"<<remote_message->getErrorCode()<<
-            "\nError Message:"<<remote_message->getErrorMessage()<<
-            "\nError Domain:"<<remote_message->getErrorDomain();
-        }
-    }
-    return 0;
-}
-
-DataPackUniquePtr consumeMessage(DataPackSharedPtr& message) {
-    DataPackUniquePtr result(new DataPack());
-    int32_t opcode = message->getInt32("opcode");
-    switch (opcode) {
-        case 0:
-            std::cout << "OPCODE 0 - Create new generation id" << std::endl;
-            result->addInt32("gen_id", 0);
-            break;
-            
-        default:
-            break;
-    }
-    return result;
-}
-
-int performIdleAction(RawDriverUnitProxy& proxy) {
-    return 0;
-}
+/*
+ //schedule work
+ switch (proxy->protocol_adapter->getConnectionState()) {
+ case ConnectionStateConnecting: {
+ std::cout << "." << std::flush;
+ break;
+ }
+ 
+ case ConnectionStateConnected:{
+ std::cout << "Connected" << std::endl;
+ break;
+ }
+ 
+ case ConnectionStateDisconnected:
+ case ConnectionStateConnectionError:
+ case ConnectionStateNotAccepted:
+ while (proxy->unit_proxy->hasMoreMessage()) {
+ std::cout << proxy->unit_proxy->getNextMessage()->message->toString();
+ }
+ if((ConnectionStateDisconnected == proxy->protocol_adapter->getConnectionState()) &&
+ connecting == false) {
+ std::cout << "Connecting" << std::flush;
+ proxy->protocol_adapter->connect();
+ connecting = true;
+ } else {
+ connecting = false;
+ std::cout << "Retry in 5 seconds"<<std::endl;
+ sleep(5);
+ }
+ break;
+ 
+ case ConnectionStateAccepted:{
+ if(connecting) {
+ std::cout << "Accepted" << std::endl;
+ connecting = false;
+ }
+ //manage the work on driver
+ if((err = executeWork(*proxy->unit_proxy))) {
+ std::cerr << "Error " << err << " during work";
+ run = false;
+ }
+ break;
+ }
+ }
+ 
+ //execute pool on connection
+ proxy->protocol_adapter->poll(100);
+ */
+//int executeWork(raw_driver::RawDriverUnitProxy& proxy) {
+//    int err = 0;
+//    switch (proxy.getAuthorizationState()) {
+//        case AuthorizationStateUnknown:
+//            authorized = false;
+//            std::cout << "Autorizing" << std::flush;
+//            proxy.authorization("work");
+//            break;
+//        case AuthorizationStateRequested:
+//            std::cout << "." << std::flush;
+//            proxy.manageAutorizationPhase();
+//            break;
+//        case AuthorizationStateDenied:
+//            std::cout << " authorization failed" << std::endl;
+//            return -1;
+//        case AuthorizationStateOk:
+//            if(authorized == false){
+//                std::cout << "Authorized" << std::endl;
+//                authorized = true;
+//            }
+//            //manage request by remote user
+//            err = manageRemoteMessage(proxy);
+//            //perform idle operation
+//            if(!err){
+//                err = performIdleAction(proxy);
+//            }
+//            break;
+//    }
+//    return err;
+//}
+//
+//int manageRemoteMessage(raw_driver::RawDriverUnitProxy& proxy) {
+//    RemoteMessageUniquePtr remote_message;
+//    DataPackUniquePtr remote_req_resp;
+//    while(proxy.hasMoreMessage()) {
+//        remote_message = proxy.getNextMessage();
+//        if(remote_message->is_request &&
+//           remote_message->request_message.get()){
+//            remote_req_resp = consumeMessage(remote_message->request_message);
+//            proxy.sendAnswer(remote_message, remote_req_resp);
+//        } else if(remote_message->isError() == false){
+//            remote_req_resp = consumeMessage(remote_message->message);
+//        } else {
+//            std::cerr << "[Remote Error]\nCode:"<<remote_message->getErrorCode()<<
+//            "\nError Message:"<<remote_message->getErrorMessage()<<
+//            "\nError Domain:"<<remote_message->getErrorDomain();
+//        }
+//    }
+//    return 0;
+//}
+//
+//DataPackUniquePtr consumeMessage(DataPackSharedPtr& message) {
+//    DataPackUniquePtr result(new DataPack());
+//    int32_t opcode = message->getInt32("opcode");
+//    switch (opcode) {
+//        case 0:
+//            std::cout << "OPCODE 0 - Create new generation id" << std::endl;
+//            result->addInt32("gen_id", 0);
+//            break;
+//
+//        default:
+//            break;
+//    }
+//    return result;
+//}
+//
+//int performIdleAction(raw_driver::RawDriverUnitProxy& proxy) {
+//    return 0;
+//}
